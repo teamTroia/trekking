9e4e44099c4e65ed4a3c1139074b9135d037893e
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-10-30 20:04:26 -0300

    Correcao erros

--- trekking.py
+++ trekking.py
@@ -7,46 +7,40 @@ gpio.setwarnings(False)
 SD1_PKS1 = 12
 IN1_PKS1 = 13
 SD2_PKS1 = 32
-IN2_PKS1 = 14
+IN2_PKS1 = 16
 
 SD1_PKS2 = 33
 IN1_PKS2 = 15
 SD2_PKS2 = 35
-IN2_PKS2 = 16
+IN2_PKS2 = 18
 
 
-pwmSD1_PKS1 = None
-pwmSD2_PKS1 = None
-pwmSD1_PKS2 = None
-pwmSD2_PKS2 = None
+gpio.setmode(gpio.BOARD)
+gpio.setup(SD1_PKS1,gpio.OUT)
+gpio.setup(SD2_PKS1,gpio.OUT)
+gpio.setup(SD1_PKS2,gpio.OUT)
+gpio.setup(SD2_PKS2,gpio.OUT)
 
-def init_gpio():
-  pio.setmode(gpio.BOARD)
-  gpio.setup(SD1_PKS1,gpio.OUT)
-  gpio.setup(SD2_PKS1,gpio.OUT)
-  gpio.setup(SD1_PKS2,gpio.OUT)
-  gpio.setup(SD2_PKS2,gpio.OUT)
-
-  gpio.setup(IN1_PKS1,gpio.OUT)
-  gpio.setup(IN2_PKS1,gpio.OUT)
-  gpio.setup(IN1_PKS2,gpio.OUT)
-  gpio.setup(IN2_PKS2,gpio.OUT)
+gpio.setup(IN1_PKS1,gpio.OUT)
+gpio.setup(IN2_PKS1,gpio.OUT)
+gpio.setup(IN1_PKS2,gpio.OUT)
+gpio.setup(IN2_PKS2,gpio.OUT)
 
 
-  pwmSD1_PKS1 = gpio.PWM(SD1_PKS1, 8000)
-  pwmSD2_PKS1 = gpio.PWM(SD2_PKS1, 8000)
-  pwmSD1_PKS2 = gpio.PWM(SD1_PKS2, 8000)
-  pwmSD2_PKS2 = gpio.PWM(SD2_PKS2, 8000)
+pwmSD1_PKS1 = gpio.PWM(SD1_PKS1, 8000)
+pwmSD2_PKS1 = gpio.PWM(SD2_PKS1, 8000)
+pwmSD1_PKS2 = gpio.PWM(SD1_PKS2, 8000)
+pwmSD2_PKS2 = gpio.PWM(SD2_PKS2, 8000)
 
-  pwmSD1_PKS1.start(0)
-  pwmSD2_PKS1.start(0)
-  pwmSD1_PKS2.start(0)
-  pwmSD2_PKS2.start(0)
+pwmSD1_PKS1.start(0)
+pwmSD2_PKS1.start(0)
+pwmSD1_PKS2.start(0)
+pwmSD2_PKS2.start(0)
 
-  gpio.output(IN1_PKS1, gpio.LOW)
-  gpio.output(IN2_PKS1, gpio.LOW)
-  gpio.output(IN1_PKS2, gpio.LOW)
-  gpio.output(IN2_PKS2, gpio.LOW)
+gpio.output(IN1_PKS1, gpio.LOW)
+gpio.output(IN2_PKS1, gpio.LOW)
+gpio.output(IN1_PKS2, gpio.LOW)
+gpio.output(IN2_PKS2, gpio.LOW)
 
 def enablePKS1(dutyCycle, direction):
   if (direction == 1):
@@ -81,3 +75,12 @@ def enablePKS2(dutyCycle, direction):
     pwmSD1_PKS2.ChangeDutyCycle(0)
     gpio.output(IN1_PKS2, gpio.LOW)
     gpio.output(IN2_PKS2, gpio.LOW)
+
+
+while(1):
+  enablePKS1(50,1)
+  time.sleep(5)
+  enablePKS1(75, 1)
+  time.sleep(5)
+  enablePKS1(25, 1)
+  time.sleep(5)
